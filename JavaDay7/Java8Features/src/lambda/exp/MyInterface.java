9d4e87562fe6c15874256f8418deeade66c93a04
javeedsir <mr.javeedsir@gmail.com>
2026-05-04 13:47:42 +0530

    Java8 Functional Interfaces,LambdaExp Types Demos,Notes

---
+++ JavaDay7/Java8Features/src/lambda/exp/MyInterface.java
@@ -0,0 +1,18 @@
+package lambda.exp;
+
+@FunctionalInterface
+public interface MyInterface {
+	
+	
+			//public int add(int a, int b);// Function functional interface
+			
+			
+			//public void setName(String name); // Consumer Functional Interface
+	
+	
+		//	public int  getCount(); // Supplier Functional Interface
+	
+			public boolean  isValidName(String name);
+			
+			
+}
