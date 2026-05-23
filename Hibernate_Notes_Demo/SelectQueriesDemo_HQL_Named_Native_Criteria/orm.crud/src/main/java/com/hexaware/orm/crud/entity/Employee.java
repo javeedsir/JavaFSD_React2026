3167d9079b18159dd318f3922e6da90d7e523b45
javeedsir <mr.javeedsir@gmail.com>
2026-05-23 15:10:43 +0530

    hql,native,named,criteria queries demos,mapping pdf

--- Hibernate_Notes_Demo/SelectQueriesDemo_HQL_Named_Native_Criteria/orm.crud/src/main/java/com/hexaware/orm/crud/entity/Employee.java
+++ Hibernate_Notes_Demo/SelectQueriesDemo_HQL_Named_Native_Criteria/orm.crud/src/main/java/com/hexaware/orm/crud/entity/Employee.java
@@ -1,4 +1,4 @@
-package com.wipro.hibernate.entity;
+package com.hexaware.orm.crud.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,69 +10,56 @@ import javax.persistence.Table;
 @Entity
 @Table(name="Emp_Table")
 
-
-@NamedQueries(
-		{
-		@NamedQuery(name="getAll" , query = "select e  from Employee e") ,
-		@NamedQuery(name="getBySalary" , query="select e  from Employee e where e.salary > ?1   and e.salary < :sal  ")
-		}
-		)
-
-public class Employee {              
-		// Entity class which associated directly with table
+// HQL named query with :amount  as a name parameter 
+@NamedQueries({
+	
+	@NamedQuery(name ="getAllEmployees"  , query ="select e  from Employee  e  where e.salary > :amount") ,
+	@NamedQuery(name="getMaxSalary" , query ="select  Max(e.salary)  from Employee e")
+	
+	
+})
+public class Employee {
 	
 	@Id
 	private int eid;
-	
 	@Column(name="emp_name")
 	private String ename;
-	
-	private double salary;
+	private  double salary;
 	
 	public Employee() {
 		
 		
 	}
-
+	
+	
 	public Employee(int eid, String ename, double salary) {
 		super();
 		this.eid = eid;
 		this.ename = ename;
 		this.salary = salary;
 	}
-
 	public int getEid() {
 		return eid;
 	}
-
 	public void setEid(int eid) {
 		this.eid = eid;
 	}
-
 	public String getEname() {
 		return ename;
 	}
-
 	public void setEname(String ename) {
 		this.ename = ename;
 	}
-
 	public double getSalary() {
 		return salary;
 	}
-
 	public void setSalary(double salary) {
 		this.salary = salary;
 	}
-
 	@Override
 	public String toString() {
 		return "Employee [eid=" + eid + ", ename=" + ename + ", salary=" + salary + "]";
 	}
-	
-	
-	
-	
 	
 	
 
