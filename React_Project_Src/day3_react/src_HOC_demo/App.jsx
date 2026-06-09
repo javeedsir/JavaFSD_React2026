156287224ba8226e3383dd901d67f50cc9baeeb0
javeedsir <mr.javeedsir@gmail.com>
2026-06-09 15:06:26 +0530

    HOC demo,notes

---
+++ React_Project_Src/day3_react/src_HOC_demo/App.jsx
@@ -0,0 +1,54 @@
+import HocExample from "./HocExample";
+import StockListComponent from "./StockListComponent";
+
+
+
+
+
+
+
+const StockData = [
+
+    {id:101,name:"TCS",price:900} ,
+    
+    { id:102, name:"Reliance" , price:500 },
+
+    {id:103,name:"Google",price:1500} ,
+
+    {id:104,name:"Facebook",price:1200} 
+
+
+];
+
+
+      const Stock =      HocExample(StockListComponent , StockData);
+
+
+export function  App(props){
+
+  
+
+        return (
+
+                <>
+                      <h1>Welcome to App Component</h1>
+                
+                    <Stock/>
+
+                   
+
+                  
+                
+                
+                </>
+
+
+
+        )
+
+
+
+
+
+
+}
